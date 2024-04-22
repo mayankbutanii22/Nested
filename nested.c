938c16b2ec922de8494cf3b775577eed398b2cc1
mayankbutanii22 <162984156+mayankbutanii22@users.noreply.github.com>
2024-04-22 17:38:39 +0530

    Add files via upload

--- nested.c
+++ nested.c
@@ -2,8 +2,12 @@
 main()
 {
 	int a,b,c;
-	printf("Enter of the first,second and third number value: ");
-	scanf("%d%d%d",&a,&b,&c);
+	printf("Enter of the first number value: ");
+	scanf("%d",&a);
+	printf("Enter of the second number value: ");
+	scanf("%d",&b);
+	printf("Enter of the third number value: ");
+	scanf("%d",&c);
 	if(a<b){
 	if(a<c){
 		printf("The Minimum value is: %d",a);
--- nested1.c
+++ nested1.c
@@ -2,20 +2,26 @@
 main()
 {
 	int a,b,c,d;
-	printf("Enter of the first,second,third and fourth value: ");
-	scanf("%d%d%d%d",&a,&b,&c,&d);
+	printf("Enter of the first value: ");
+	scanf("%d",&a);
+	printf("Enter of the second value: ");
+	scanf("%d",&b);
+	printf("Enter of the third value: ");
+	scanf("%d",&c);
+	printf("Enter of the fourth value: ");
+	scanf("%d",&d);
 	if(a>b){
 	if(a>c){
 	if(a>d)	
 		printf("The maximum value is: %d",a);
 	}
-	if(b>c){
+	else if(b>c){
 		printf("The maximum value is: %d",b);
 	}
-	if(b>d){
+	else if(b>d){
 		printf("The maximum value is: %d",d);
 	}else{
 		printf("The maximum value is: %d",c);
 	}	
-	}
+}
 }
